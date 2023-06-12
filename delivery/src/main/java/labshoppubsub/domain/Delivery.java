e5cdfdb41bb54334e147db45c5dec9c617623aea
kimyoukyoung <104448470+kimyoukyoung@users.noreply.github.com>
2023-06-12 21:13:43 +0900

    commit

---
+++ delivery/src/main/java/labshoppubsub/domain/Delivery.java
@@ -0,0 +1,50 @@
+package labshoppubsub.domain;
+
+import java.util.Date;
+import java.util.List;
+import javax.persistence.*;
+import labshoppubsub.DeliveryApplication;
+import lombok.Data;
+
+@Entity
+@Table(name = "Delivery_table")
+@Data
+public class Delivery {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String customerId;
+
+    private String orderId;
+
+    public static DeliveryRepository repository() {
+        DeliveryRepository deliveryRepository = DeliveryApplication.applicationContext.getBean(
+            DeliveryRepository.class
+        );
+        return deliveryRepository;
+    }
+
+    public static void addToDeliveryList(OrderPlaced orderPlaced) {
+        //implement business logic here:
+
+        /** Example 1:  new item 
+        Delivery delivery = new Delivery();
+        repository().save(delivery);
+
+        */
+
+        /** Example 2:  finding and process
+        
+        repository().findById(orderPlaced.get???()).ifPresent(delivery->{
+            
+            delivery // do something
+            repository().save(delivery);
+
+
+         });
+        */
+
+    }
+}
